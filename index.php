6c8914bf157e5f1220592d38674196c16bbce19e
scristo2 <sergio_colino_c@hotmail.com>
2022-06-22 23:02:10 +0200

    update

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ header("Connection: close");
     <link rel="stylesheet" href="/font-awesome-4.7.0/css/font-awesome.min.css">
     <script src="/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="/jquery.js"></script>
+    <script src="/src/index.js" defer></script>
 </head>
 
 <body>
@@ -77,43 +78,14 @@ header("Connection: close");
             </nav>
         </header>
         <main class="p-4">
-            <div class="container">
-
-                <div class="row height d-flex justify-content-center align-items-center">
-
-                    <div class="col-md-8">
-
-                        <div class="search">
-                            <i class="fa fa-search"></i>
-                            <input type="text" class="form-control" placeholder="¿Que buscas?">
-                            <button class="btn btn-primary">Buscar</button>
-                        </div>
-
-                    </div>
+            <div class="search">
+                <i class="fa fa-search"></i>
+                <input type="text" id="search" class="form-control" placeholder="¿Que buscas?">
+                <button class="btn btn-primary">Buscar</button>
+                <div class="form-control d-none" id="result">
+                    <ul id="result-ul"></ul>
                 </div>
-                 <!---->
-                <div class="row height d-flex justify-content-center align-items-center">
-                    <div class="col-md-6">
-                        <div class="matches">
-                            <section class="mb-4">
-                                <ul class="match-ul p-4">
-                                    <li class="match-li">
-                                        <p><i class="fa fa-star text-warning"></i><span class="fw-bold text-decoration-underline"> Lo mas buscado</span></p>
-                                    </li>
-                                    <li class="text-align-center li-more-visited p-3"><h5><i class="fa fa-bus text-primary"></i><span class="li-more-visited-product"> Tractores</span></h5></li>
-                                    <li class="text-align-center li-more-visited p-3"><h5><i class="fa fa-bath text-success"></i><span class="li-more-visited-product"> Muebles</span></h5></li>
-                                    <li class="text-align-center li-more-visited p-3"><h5><i class="fa fa-gamepad text-warning"></i><span class="li-more-visited-product"> Play Station 5</span></h5></li>
-                                    <li class="text-align-center li-more-visited p-3"><h5><i class="fa fa-ship text-danger"></i><span class="li-more-visited-product"> Barcos</span></h5></li>
-                                    <li class="text-align-center li-more-visited p-3"><h5><i class="fa fa-car text-info"></i><span class="li-more-visited-product"> Sofas</span></h5></li>
-                                    <li class="text-align-center li-more-visited p-3"><h5><i class="fa fa-bicycle text-muted"></i><span class="li-more-visited-product"> Patinetes electricos</span></h5></li>
-                                </ul>
-                            </section>
-                        </div>
-                    </div>
-                </div>
-                <!---->
             </div>
-            <!--end container inside main-->
         </main>
         <!-- Footer -->
         <footer class="bg-dark text-center text-white">
---
+++ src/index.js
@@ -0,0 +1,33 @@
+class Index {
+
+
+    //write on input search
+
+    inputSearch(){
+         
+         let aa = ["mercedes djpsod ddud0'", "reanualt", "audi", "bmw", "opel", "seat"];
+         
+         $("#search").on("input", () => {
+            
+            aa.map((e, i) => {
+
+                if($("#search").val().length > 0){
+
+                    $("#result-ul").append(`<li class="mt-4" id="result-li">${e}</li>`);
+                    $("#result").removeClass("d-none");
+                }else{
+
+                    $("#result-ul").empty();
+                    $("#result").addClass("d-none");
+                }
+            });
+        });
+    }
+}
+
+const index = new Index();
+
+$(document).ready(() => {
+
+    index.inputSearch();
+});
